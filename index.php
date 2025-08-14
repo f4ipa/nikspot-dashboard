97b8d0211572dd931254ba5a94bbe250a828110a
Guillaume F4IPA <f4ipa@free.fr>
2025-08-14 17:29:43 +0200

    Nettoyage de la page index.php

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<?php
+<?php require('header.php') ?>
 
-$content = file_get_contents('/etc/svxlink/svxlink.conf');
-preg_match('/^CALLSIGN=(.*)$/m', $content, $matches);
-$callsign = $matches[1] ?? '';
-
-if (isset($_POST['callsign'])) {
-    $callsign = trim($_POST['callsign']);
-    $content = preg_replace('/^CALLSIGN=.*/m', "CALLSIGN=$callsign", $content);
-    file_put_contents('/etc/svxlink/svxlink.conf', $content);
-    system('service svxlink restart');
-}
-
-require('header.php') 
-?>
-
-    <form action="" method="POST">
-        <h4 class="mb-3">Votre indicatif</h4>
-        <input type="text" name="callsign" class="form-control mb-3" value="<?= $callsign ?>">
-        <button type="submit" class="btn btn-primary">Enregistrer</button>
-    </form>
+<p>Le contenu de cette page arrive bientôt...</p>
     
 <?php require('footer.php') ?>
